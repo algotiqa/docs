938fa3a41366174fa418a4744937e9c6e3a2f144
acarboni <andrea.carboni71@gmail.com>
2026-01-18 16:59:26 +0100

    Commit to rename repo

---
+++ tsl/tsl.mm
@@ -0,0 +1,499 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks>
+    <bookmark nodeId="ID_1090958577" name="Nodo radice" opensAsRoot="true"/>
+</bookmarks>
+<node TEXT="TSL" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1764670066671" VGAP_QUANTITY="12.75 pt">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8ff">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_nord_template.mm" show_tags="UNDER_NODES" showTagCategories="false"/>
+    <tags category_separator="::" tagcolor1="Info#00ed5fff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Statement" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="bottom_or_right" ID="ID_1937311939" CREATED="1764674428303" MODIFIED="1764927801944" HGAP_QUANTITY="-305.24999 pt" VSHIFT_QUANTITY="-113.25 pt">
+<hook NAME="FreeNode"/>
+<node TEXT="VarDeclaration" ID="ID_1659967403" CREATED="1764674469373" MODIFIED="1764674508520"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Name&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;string
+    </p>
+    <p>
+      Type&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;Type
+    </p>
+    <p>
+      Expression Expression
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="VarAssignment" ID="ID_1644346986" CREATED="1764674524380" MODIFIED="1764674560503"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Chain&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;*ChainedExpression
+    </p>
+    <p>
+      Expression Expression
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="If" ID="ID_4391895" CREATED="1764674602740" MODIFIED="1764674617406"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      condBlocks []*ConditionalBlock
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Return" ID="ID_481444290" CREATED="1764674631836" MODIFIED="1764674644106"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      values []Expression
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="FunctionCall" ID="ID_425248135" CREATED="1764674666716" MODIFIED="1764674669753"/>
+<node TEXT="FunctionCallAssignment" ID="ID_1440469819" CREATED="1764674679556" MODIFIED="1764674687316"/>
+</node>
+<node TEXT="Expression" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="bottom_or_right" ID="ID_1453234030" CREATED="1764671219721" MODIFIED="1764930289386" HGAP_QUANTITY="163.5 pt" VSHIFT_QUANTITY="112.5 pt">
+<hook NAME="FreeNode"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ResolveType(s Scope, depth int)&#xa0;&#xa0;&#xa0;Type,error
+    </p>
+    <p>
+      Info() *parser.Info
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="Arithmetic" ID="ID_1277589680" CREATED="1764673387752" MODIFIED="1765214395432"/>
+<node TEXT="BarAccess" ID="ID_1724450027" CREATED="1764673471839" MODIFIED="1764673516824"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Bar&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;int
+    </p>
+    <p>
+      Accessor Expression
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Chained" ID="ID_53189503" CREATED="1764673557999" MODIFIED="1764674368458"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      HasThis bool
+    </p>
+    <p>
+      Items&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;[]ChainItem
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Constant" ID="ID_1533541422" CREATED="1764673561239" MODIFIED="1764673574624"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Value Value
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="FunctionCall" ID="ID_1326819802" CREATED="1764673768734" MODIFIED="1764673819905" BACKGROUND_COLOR="#ffffcc"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Name&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;string
+    </p>
+    <p>
+      Expressions []Expression
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="And" ID="ID_620629379" CREATED="1764673873782" MODIFIED="1764673884120"/>
+<node TEXT="Or" ID="ID_334299537" CREATED="1764673884477" MODIFIED="1764673885977"/>
+<node TEXT="Not" ID="ID_1158456349" CREATED="1764673886469" MODIFIED="1764673888294"/>
+<node TEXT="Relational" ID="ID_1713262884" CREATED="1764673996158" MODIFIED="1765214439050"/>
+<node TEXT="List" ID="ID_897987896" CREATED="1764674189541" MODIFIED="1764674230482"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Value&#xa0;&#xa0;*ListValue
+    </p>
+    <p>
+      Values []Expression
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Map" ID="ID_247400611" CREATED="1764674256044" MODIFIED="1764674302776"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Value *MapValue
+    </p>
+    <p>
+      Values map[Value]Expression
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="Type" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="bottom_or_right" ID="ID_315477129" CREATED="1764669506780" MODIFIED="1764671198937" HGAP_QUANTITY="-594.74998 pt" VSHIFT_QUANTITY="-103.5 pt">
+<hook NAME="FreeNode"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Id()&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;int8
+    </p>
+    <p>
+      String() string
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="int" POSITION="bottom_or_right" ID="ID_1132993659" CREATED="1764667816688" MODIFIED="1764670170783" HGAP_QUANTITY="-86.5 pt" VSHIFT_QUANTITY="-131.25 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="enum" POSITION="bottom_or_right" ID="ID_64226429" CREATED="1764667795832" MODIFIED="1764670184561" TAGS="Info" HGAP_QUANTITY="131 pt" VSHIFT_QUANTITY="-112.5 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="bool" POSITION="bottom_or_right" ID="ID_169944237" CREATED="1764667778711" MODIFIED="1764670167874" HGAP_QUANTITY="-94 pt" VSHIFT_QUANTITY="-93 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="list" POSITION="bottom_or_right" ID="ID_178048983" CREATED="1764667821719" MODIFIED="1764670664581" HGAP_QUANTITY="131.75 pt" VSHIFT_QUANTITY="-45.75 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="real" POSITION="bottom_or_right" ID="ID_161252900" CREATED="1764667828767" MODIFIED="1764670166147" HGAP_QUANTITY="-91 pt" VSHIFT_QUANTITY="-54 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="string" POSITION="bottom_or_right" ID="ID_699957882" CREATED="1764667830408" MODIFIED="1764670163391" HGAP_QUANTITY="-104.5 pt" VSHIFT_QUANTITY="-18.75 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="map" POSITION="bottom_or_right" ID="ID_1618457695" CREATED="1764667825535" MODIFIED="1764670668056" HGAP_QUANTITY="132.5 pt" VSHIFT_QUANTITY="12.75 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="date" POSITION="bottom_or_right" ID="ID_1266008190" CREATED="1764667792847" MODIFIED="1764670161575" HGAP_QUANTITY="-97.75 pt" VSHIFT_QUANTITY="21 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="time" POSITION="bottom_or_right" ID="ID_1989399145" CREATED="1764667834424" MODIFIED="1764670158682" HGAP_QUANTITY="-96.25 pt" VSHIFT_QUANTITY="60 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="class" POSITION="bottom_or_right" ID="ID_616807323" CREATED="1764667789232" MODIFIED="1764670176298" TAGS="Info" HGAP_QUANTITY="122.75 pt" VSHIFT_QUANTITY="67.5 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="timeseries" POSITION="bottom_or_right" ID="ID_197893970" CREATED="1764667836320" MODIFIED="1764670153309" HGAP_QUANTITY="-137.5 pt" VSHIFT_QUANTITY="98.25 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="To find out" POSITION="bottom_or_right" ID="ID_1131656071" CREATED="1764667842120" MODIFIED="1764670174132" TAGS="Info" HGAP_QUANTITY="119.75 pt" VSHIFT_QUANTITY="134.25 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="error" POSITION="bottom_or_right" ID="ID_1509530461" CREATED="1764667799367" MODIFIED="1764670151609" HGAP_QUANTITY="-103 pt" VSHIFT_QUANTITY="141 pt">
+<hook NAME="FreeNode"/>
+</node>
+</node>
+<node TEXT="Value" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="bottom_or_right" ID="ID_473200727" CREATED="1764670209072" MODIFIED="1764927675611" HGAP_QUANTITY="-614.24998 pt" VSHIFT_QUANTITY="264.74999 pt">
+<hook NAME="FreeNode"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Data() any
+    </p>
+    <p>
+      Type() Type
+    </p>
+    <p>
+      Equals&#xa0;&#xa0;&#xa0;&#xa0;(other Value) bool
+    </p>
+    <p>
+      LessThan(other Value) bool
+    </p>
+    <p>
+      String() string
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="int" ID="ID_1377472260" CREATED="1764670330075" MODIFIED="1764670785718" HGAP_QUANTITY="-92.5 pt" VSHIFT_QUANTITY="-155.25 pt"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      int64
+    </p>
+  </body>
+</html></richcontent>
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="bool" ID="ID_247085625" CREATED="1764670438779" MODIFIED="1764670782730" HGAP_QUANTITY="-88.75 pt" VSHIFT_QUANTITY="-101.25 pt"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      bool
+    </p>
+  </body>
+</html></richcontent>
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="list" ID="ID_707491369" CREATED="1764670934496" MODIFIED="1764671154291" HGAP_QUANTITY="205.24999 pt" VSHIFT_QUANTITY="-99 pt">
+<hook NAME="FreeNode"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      values []Value
+    </p>
+    <p>
+      type_&#xa0;&#xa0;&#xa0;Type
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="real" ID="ID_1278743284" CREATED="1764670471307" MODIFIED="1764670779877" HGAP_QUANTITY="-94.75 pt" VSHIFT_QUANTITY="-48 pt"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      float64
+    </p>
+  </body>
+</html></richcontent>
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="map" ID="ID_365546455" CREATED="1764670937080" MODIFIED="1764671146932" HGAP_QUANTITY="203.74999 pt" VSHIFT_QUANTITY="-9.75 pt">
+<hook NAME="FreeNode"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      values&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;map[Value]Value
+    </p>
+    <p>
+      keyType&#xa0;&#xa0;&#xa0;&#xa0;Type
+    </p>
+    <p>
+      valueType Type
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="string" ID="ID_1177393156" CREATED="1764670496172" MODIFIED="1764670777827" HGAP_QUANTITY="-99.25 pt" VSHIFT_QUANTITY="2.25 pt"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      string
+    </p>
+  </body>
+</html></richcontent>
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="enum" ID="ID_1600426053" CREATED="1764670927776" MODIFIED="1764671151853" BACKGROUND_COLOR="#ffffcc" HGAP_QUANTITY="214.24999 pt" VSHIFT_QUANTITY="86.25 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="date" ID="ID_393409485" CREATED="1764670537340" MODIFIED="1764670774173" HGAP_QUANTITY="-111.25 pt" VSHIFT_QUANTITY="54.75 pt"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      *data.Date
+    </p>
+  </body>
+</html></richcontent>
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="class" ID="ID_1990704493" CREATED="1764670939873" MODIFIED="1764671157008" BACKGROUND_COLOR="#ffffcc" HGAP_QUANTITY="214.99999 pt" VSHIFT_QUANTITY="134.25 pt">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="time" ID="ID_1135287952" CREATED="1764670545403" MODIFIED="1764670772380" HGAP_QUANTITY="-115 pt" VSHIFT_QUANTITY="111.75 pt"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      *data.Time
+    </p>
+  </body>
+</html></richcontent>
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="timeseries" ID="ID_1833303803" CREATED="1764670743698" MODIFIED="1764671160471" HGAP_QUANTITY="215.74999 pt" VSHIFT_QUANTITY="176.99999 pt" BACKGROUND_COLOR="#ffffcc">
+<hook NAME="FreeNode"/>
+</node>
+<node TEXT="error" ID="ID_1210492404" CREATED="1764670564659" MODIFIED="1764671163982" HGAP_QUANTITY="-103.75 pt" VSHIFT_QUANTITY="176.99999 pt"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      string
+    </p>
+  </body>
+</html></richcontent>
+<hook NAME="FreeNode"/>
+</node>
+</node>
+<node TEXT="Symbol" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="bottom_or_right" ID="ID_1760171488" CREATED="1764779152591" MODIFIED="1764927806122" HGAP_QUANTITY="-178.49999 pt" VSHIFT_QUANTITY="313.49999 pt">
+<hook NAME="FreeNode"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Name() string
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="VariableSymbol" ID="ID_954166308" CREATED="1764779169666" MODIFIED="1764779180586"/>
+<node TEXT="ConstantSymbol" ID="ID_1024466588" CREATED="1764779181602" MODIFIED="1764779186324"/>
+<node TEXT="TypeSymbol" ID="ID_601235235" CREATED="1764779187897" MODIFIED="1764779371717"/>
+<node TEXT="FunctionSymbol" ID="ID_1293320711" CREATED="1764779205186" MODIFIED="1764779210886"/>
+</node>
+</node>
+</map>
